3f77da00f3694b4e53e1902ac97fa2881613b646
Eric Shih <eshih727@gmail.com>
2024-04-16 18:30:18 -0700

    Add form hiding, border dynamic coloring, loading spinner

--- src/food.js
+++ src/food.js
@@ -6,15 +6,27 @@
 			search();
 		});
 
-		$("#restaurant").on("click", function() {
+		$("#picture").on("swipeleft", function() {
+			restaurantIndex--;
+			showRestaurant(getRestaurantDetails(restaurantIndex));
+		});
+
+		$("#picture").on("click", function() {
+			showRestaurant(getRestaurantDetails(restaurantIndex));
+		});
+
+		$("#picture").on("swiperight", function() {
 			showRestaurant(getRestaurantDetails(restaurantIndex));
 		});
 	};
 
 	function search() {
+		$("#forms").hide();
+		$("#loading").show();
 		fetch('https://shihe.github.io/food/src/json/searchMock.json')
 			.then((response) => response.json())
 			.then((json) => sessionStorage.setItem("searchResponse", JSON.stringify(json)));
+		$("#loading").hide();
 		restaurantIndex = 0;
 		showRestaurant(getRestaurantDetails(restaurantIndex));
 	}
@@ -24,27 +36,49 @@
 	}
 
 	function showRestaurant(restaurantDetails) {
-	  	console.log(restaurantDetails);
-	  	$("#title").attr("href", restaurantDetails.url);
-	  	$("#title").text(restaurantDetails.name);
+		console.log(restaurantDetails);
+		$("#title").attr("href", restaurantDetails.url);
+		$("#title").text(restaurantDetails.name);
 
-	  	const ratingStr = constructRatingStr(restaurantDetails.rating, restaurantDetails.review_count);
-  		$("#rating").text(ratingStr);
+		const weightedRating = getWeightedRating(restaurantDetails.rating, restaurantDetails.review_count);
+		const ratingStr = "Score: " + weightedRating +
+			" / Rating: " + restaurantDetails.rating +
+			" / Reviews: " + restaurantDetails.review_count;
+		$("#rating").text(ratingStr);
 
-  		const categoryStr = restaurantDetails.categories.map((category) => category.title).join(", ");
-  		$("#categories").text(categoryStr);
-  		$("#price").text(restaurantDetails.price);
-	  	let cardImg = document.createElement("img");
-	  	cardImg.src = restaurantDetails.image_url;
-	  	$("#picture").html(cardImg);
+		const categoryStr = restaurantDetails.categories.map((category) => category.title).join(", ");
+		$("#categories").text(categoryStr);
+		$("#price").text(restaurantDetails.price);
 
-	  	$("#restaurant").show();
-	  	restaurantIndex++;
+		let cardImg = document.createElement("img");
+		cardImg.src = restaurantDetails.image_url;
+		$("#picture").html(cardImg);
+
+		const borderColor = "5px solid " + getBorderColor(weightedRating);
+		$("#restaurant").css({"border": borderColor});
+
+		$("#restaurant").show();
+		restaurantIndex++;
 	};
 
-	function constructRatingStr(rating, review_count) {
-		const weightedRating = Math.trunc(rating * Math.min(20, (Math.log(review_count) * 3)));
-		return "Score: " + weightedRating + " / Rating: " + rating + " / Reviews: " + review_count;
+	function getWeightedRating(rating, review_count) {
+		return Math.trunc(rating * Math.min(20, (Math.log(review_count) * 3)));
+	}
+
+	function getBorderColor(weightedRating) {
+		if (weightedRating < 50) {
+			return "black";
+		} else if (weightedRating < 60) {
+			return "sienna";
+		} else if (weightedRating < 70) {
+			return "grey";
+		} else if (weightedRating < 80) {
+			return "gold";
+		} else if (weightedRating < 90) {
+			return "dodgerblue";
+		} else {
+			return "crimson";
+		}
 	}
 
 }) (jQuery, window);
