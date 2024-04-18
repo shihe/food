aab60c4a3d329df70e2cc6f765a2912a3f443486
Eric Shih <eshih727@gmail.com>
2024-04-18 10:33:12 -0700

    Add bootstrap, dynamic restaurant card generation

--- src/food.js
+++ src/food.js
@@ -1,102 +1,85 @@
 (function($, window) {
-	var restaurantIndex = 0;
-	window.onload = () => {
-		console.log("Hello");
-		$("#search").on("click", function() {
-			search();
-		});
+    window.onload = () => {
+        console.log("Hello");
+        $("#search").on("click", function() {
+            search();
+        });
+    };
 
-		// Detect if the device is mobile
-		var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    function search() {
+        $("#loading").show();
+        fetch('https://shihe.github.io/food/src/json/searchMock.json').then((response) => response.json()).then((json) => sessionStorage.setItem("searchResponse", JSON.stringify(json)));
+        $("#loading").hide();
+        showRestaurant(getRestaurants());
+    }
 
-		// If the device is mobile, use the swipe event listener
-		if (isMobile) {
-			$("#picture").on("swipeleft", function() {
-				if (restaurantIndex >= 2) {
-					restaurantIndex -= 2;
-					showRestaurant(getRestaurantDetails(restaurantIndex));
-				}
-			});
+    function getRestaurants() {
+        return JSON.parse(sessionStorage.getItem("searchResponse")).businesses;
+    }
 
-			$("#picture").on("swiperight", function() {
-				showRestaurant(getRestaurantDetails(restaurantIndex));
-			});
-		} else {
-			$("#picture").on("click", function() {
-				showRestaurant(getRestaurantDetails(restaurantIndex));
-			});
-		}
+    function showRestaurant(restaurants) {
+        for (let i = 0; i < restaurants.length; i++) {
+            let restaurantDetails = restaurants[i];
+            console.log(restaurantDetails, i);
+            const carouselItem = constructCarouselItem(restaurantDetails);
+            $('#restaurant > div').append(carouselItem);
+        }
+        $('#restaurant > div').find('.carousel-item').first().addClass('active');
+        $("#restaurant").carousel();
+        $("#restaurant").show();
+    };
 
-	  	var slider = tns({
-			"container": ".slider",
-			"items": 3,
-			"slideBy": "page",
-			"mouseDrag": true,
-			"swipeAngle": false,
-			"speed": 400
-		});
-	};
+    function constructCarouselItem(restaurantDetails) {
+        const item = $('<div>', {
+            'class': 'carousel-item'
+        });
+        const img = $('<img>', {
+            'class': 'd-block w-100'
+        }).attr('src', restaurantDetails.image_url);
+        item.append(img);
+        const itemCaption = $('<div>', {
+            'class': 'carousel-caption d-block'
+        });
+        const titleHeading = $('<h5>');
+        const title = $('<a>', {
+            'class': 'details'
+        }).attr('href', restaurantDetails.url).text(restaurantDetails.name);
+        titleHeading.append(title);
+        const itemDetails = $('<div>', {
+            'class': 'details'
+        });
+        const weightedRating = getWeightedRating(restaurantDetails.rating, restaurantDetails.review_count);
+        const ratingStr = "Score: " + weightedRating + " / Rating: " + restaurantDetails.rating + " / Reviews: " + restaurantDetails.review_count;
+        const categoryStr = restaurantDetails.categories.map((category) => category.title).join(", ");
+        const itemDetailsStr = ratingStr + "                  " + categoryStr + "                  " + restaurantDetails.price;
+        itemDetails.text(itemDetailsStr);
+        itemCaption.append(titleHeading);
+        itemCaption.append(itemDetails);
+        item.append(itemCaption);
+        const borderColor = "5px solid " + getBorderColor(weightedRating);
+        item.css({
+            "border": borderColor
+        });
+        return item;
+    }
 
-	function search() {
-		$("#forms").hide();
-		$("#loading").show();
-		fetch('https://shihe.github.io/food/src/json/searchMock.json')
-			.then((response) => response.json())
-			.then((json) => sessionStorage.setItem("searchResponse", JSON.stringify(json)));
-		$("#loading").hide();
+    function getWeightedRating(rating, review_count) {
+        return Math.trunc(rating * Math.min(20, (Math.log(review_count) * 3)));
+    }
 
-		restaurantIndex = 0;
-		showRestaurant(getRestaurantDetails(restaurantIndex));
-	}
-
-	function getRestaurantDetails(restaurantIndex) {
-		return JSON.parse(sessionStorage.getItem("searchResponse")).businesses[restaurantIndex];
-	}
-
-	function showRestaurant(restaurantDetails) {
-		console.log(restaurantDetails);
-		$("#title").attr("href", restaurantDetails.url);
-		$("#title").text(restaurantDetails.name);
-
-		const weightedRating = getWeightedRating(restaurantDetails.rating, restaurantDetails.review_count);
-		const ratingStr = "Score: " + weightedRating +
-			" / Rating: " + restaurantDetails.rating +
-			" / Reviews: " + restaurantDetails.review_count;
-		$("#rating").text(ratingStr);
-
-		const categoryStr = restaurantDetails.categories.map((category) => category.title).join(", ");
-		$("#categories").text(categoryStr);
-		$("#price").text(restaurantDetails.price);
-
-		let cardImg = document.createElement("img");
-		cardImg.src = restaurantDetails.image_url;
-		$("#picture").html(cardImg);
-
-		const borderColor = "5px solid " + getBorderColor(weightedRating);
-		$("#restaurant").css({"border": borderColor});
-
-		$("#restaurant").show();
-		restaurantIndex++;
-	};
-
-	function getWeightedRating(rating, review_count) {
-		return Math.trunc(rating * Math.min(20, (Math.log(review_count) * 3)));
-	}
-
-	function getBorderColor(weightedRating) {
-		if (weightedRating < 50) {
-			return "black";
-		} else if (weightedRating < 60) {
-			return "sienna";
-		} else if (weightedRating < 70) {
-			return "grey";
-		} else if (weightedRating < 80) {
-			return "gold";
-		} else if (weightedRating < 90) {
-			return "dodgerblue";
-		} else {
-			return "crimson";
-		}
-	}
-
-}) (jQuery, window);
+    function getBorderColor(weightedRating) {
+        if (weightedRating < 50) {
+            return "black";
+        } else if (weightedRating < 60) {
+            return "sienna";
+        } else if (weightedRating < 70) {
+            return "grey";
+        } else if (weightedRating < 80) {
+            return "gold";
+        } else if (weightedRating < 90) {
+            return "dodgerblue";
+        } else {
+            return "crimson";
+        }
+    }
+})(jQuery, window);
